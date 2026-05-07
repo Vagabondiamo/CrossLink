6a4fe19dc0345be084765286fc712ddfa53062c1
Zakaria <157507566+Vagabondiamo@users.noreply.github.com>
2026-05-07 10:22:05 +0200

    Fix: Import Label instead of QLabel in main.py

--- mobile/main.py
+++ mobile/main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
-from kivy.uix.label import QLabel
+from kivy.uix.label import Label
 
 class CrossLinkBareBones(App):
     def build(self):
-        return QLabel(text="CROSS-LINK V0.5\nIf you see this, Kivy is working!\nNow we know the problem is in the other files.")
+        return Label(text="CROSS-LINK V0.5\nIf you see this, Kivy is working!\nNow we know the problem is in the other files.")
 
 if __name__ == '__main__':
     CrossLinkBareBones().run()
